23082d296c4357d8f865e7e9619f55321043c8f5
Andrea Fontanelli 71594 <andrea1.fontanelli@consulenti.csi.it>
2020-12-01 15:52:24 +0100

    primo servizio di prova infrastruttura implementato

--- staon-csi/src/test/java/it/csi/stacore/staon/test/servizioconsultazione/ServizioConsultazioneTestCase.java
+++ staon-csi/src/test/java/it/csi/stacore/staon/test/servizioconsultazione/ServizioConsultazioneTestCase.java
@@ -1,13 +1,63 @@
 package it.csi.stacore.staon.test.servizioconsultazione;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
-public class ServizioConsultazioneTestCase {
+import it.csi.csi.porte.InfoPortaDelegata;
+import it.csi.csi.porte.proxy.PDProxy;
+import it.csi.csi.util.xml.PDConfigReader;
+import it.csi.finco.interfacecsi.FincoFPGestioneCommissioneService;
+import it.csi.stacore.staon.interfacecsi.ServizioConsultazione;
+import junit.framework.TestCase;
+
+public class ServizioConsultazioneTestCase extends TestCase{
+
+	private InfoPortaDelegata infoPortaDelegata = null;
 
 
+	private ServizioConsultazione service;
+
+	protected InfoPortaDelegata configuraPortaDelegata() throws Exception {
+		String method = "configuraPortaDelegata";
+
+		InfoPortaDelegata info = PDConfigReader.read("src/test/resources/pd/defPD_ServizioConsultazione.xml");
+		InfoPortaDelegata[] infoPortaDelegataArray =  info.getPlugins();
+		InfoPortaDelegata portaDelegata = infoPortaDelegataArray[0];
+
+		//Tracer.info(logger, getClass().getName(), method, "urlPa= " + urlPa);
+		//Tracer.info(logger, getClass().getName(), method, "portaDelegata.getUrlPortaApplicativa()= " + portaDelegata.getUrlPortaApplicativa());
+
+		//String urlPortaApplicativa = StringUtils.replace(portaDelegata.getUrlPortaApplicativa(), URL_PA, urlPa);
+
+		//Tracer.info(logger, getClass().getName(), method, "urlPortaApplicativa= " + urlPortaApplicativa);
+
+		//portaDelegata.setUrlPortaApplicativa(urlPortaApplicativa);
+
+
+		return info;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	public void setUp() throws Exception {
+		try {
+			System.out.println("setUp");
+			//Log4jConfigurer.initLogging( "classpath:log4j.properties" );
+			//loadProperties();
+			infoPortaDelegata = configuraPortaDelegata();
+
+			service = (ServizioConsultazione) PDProxy.newInstance(infoPortaDelegata);
+
+
+			System.out.println("service load correctly");
+		}
+		catch(Exception e) {
+			System.out.println("Exception " + e);
+		}
+
+
+	}
 
 
 	@Test
@@ -17,9 +67,9 @@ public class ServizioConsultazioneTestCase {
 
 
 		try {
+			boolean result = service.testResources();
 
-
-			assertTrue(true);
+			assertTrue(result);
 		} catch (Exception e) {
 			//LOG.error("", e);
 			//Tracer.error(LOG, getClass().getSimpleName(), method, "Exception: " + e);
